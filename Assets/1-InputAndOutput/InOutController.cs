370b582ff9347aff838b5d5cbb78c3e36706d308
D. Suzuki <bboydaisuke@gmail.com>
2021-04-27 23:03:10 +0900

    １週目の内容を追加した

---
+++ Assets/1-InputAndOutput/InOutController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InOutController : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("開始");
+    }
+
+    void Update()
+    {
+        // 方向の入力を検出する
+
+        float h = Input.GetAxisRaw("Horizontal");  // h は Horizontal（水平）の h
+        float v = Input.GetAxisRaw("Vertical");    // v は Vertical（垂直）の v
+
+        if (h != 0 || v != 0)
+        {
+            Debug.Log("x: " + h.ToString() + ", y: " + v.ToString());
+        }
+
+        // ジャンプボタンを検出する
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump");
+        }
+
+        // T キーを検出する
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            Debug.Log("Tが押されている");
+        }
+
+        // マウスボタンを検出する
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("左クリックが離された");
+        }
+    }
+}
---
+++ Assets/1-InputAndOutput/VariableController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableController : MonoBehaviour
+{
+    void Start()
+    {
+        // 変数と型（組み込み型）
+
+        int a = 2;
+        Debug.Log(a);
+
+        float b = 1.5f;
+        Debug.Log(b);
+
+        string c = "abc";   // 文字列は " （ダブルクォーテーション、二重引用符）で囲む
+        Debug.Log(c);
+
+        string d = "def";
+        Debug.Log(d);
+
+        //int f = a + b;  // これはできない
+        //Debug.Log(f);
+
+        float g = a + b;
+        Debug.Log(g);
+
+        string h = c + d;   // 文字列も足し算できる
+        Debug.Log(h);
+
+        //string i = 2 * c;   // 文字列の掛け算は（C# では）できない
+        //Debug.Log(i);
+
+        // 以降に課題のコードを記述せよ。
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
