c021f53de8e38dd7f80d3524ef6503020227d087
D. Suzuki <bboydaisuke@gmail.com>
2021-05-26 00:08:41 +0900

    3週目の内容を追加した

---
+++ Assets/3-Components/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 弾丸を制御するコンポーネント
+/// </summary>
+public class BulletController : MonoBehaviour
+{
+    /// <summary>弾が飛ぶ速さ</summary>
+    [SerializeField] float m_speed = 3f;
+    /// <summary>弾の生存期間（秒）</summary>
+    [SerializeField] float m_lifeTime = 5f;
+
+    void Start()
+    {
+        // 右方向に飛ばす
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.right * m_speed;
+        // 生存期間が経過したら自分自身を破棄する
+        Destroy(this.gameObject, m_lifeTime);
+    }
+}
---
+++ Assets/3-Components/GunmanController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ガンマンのキャラクターを操作するコンポーネント
+/// </summary>
+public class GunmanController : MonoBehaviour
+{
+    /// <summary>左右移動する力</summary>
+    [SerializeField] float m_movePower = 5f;
+    /// <summary>ジャンプする力</summary>
+    [SerializeField] float m_jumpPower = 15f;
+    /// <summary>色の配列</summary>
+    [SerializeField] Color[] m_colors = default;
+    /// <summary>弾丸のプレハブ</summary>
+    [SerializeField] GameObject m_bulletPrefab = default;
+    /// <summary>銃口の位置を設定するオブジェクト</summary>
+    [SerializeField] Transform m_muzzle = default;
+    /// <summary>入力に応じて左右を反転させるかどうかのフラグ</summary>
+    [SerializeField] bool m_flipX = false;
+    Rigidbody2D m_rb = default;
+    SpriteRenderer m_sprite = default;
+    /// <summary>m_colors に使う添字</summary>
+    int m_colorIndex;
+    /// <summary>水平方向の入力値</summary>
+    float m_h;
+    float m_scaleX;
+    /// <summary>最初に出現した座標</summary>
+    Vector3 m_initialPosition;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_sprite = GetComponent<SpriteRenderer>();
+        // 初期位置を覚えておく
+        m_initialPosition = this.transform.position;
+    }
+
+    void Update()
+    {
+        // 入力を受け取る
+        m_h = Input.GetAxisRaw("Horizontal");
+
+        // 各種入力を受け取る
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("ここにジャンプする処理を書く。");
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("ここに弾を発射する処理を書く。");
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Debug.Log("ここに色を切り替える処理を書く。");
+        }
+
+        // 下に行きすぎたら初期位置に戻す
+        if (this.transform.position.y < -10f)
+        {
+            this.transform.position = m_initialPosition;
+        }
+
+        // 設定に応じて左右を反転させる
+        if (m_flipX)
+        {
+            FlipX(m_h);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // 力を加えるのは FixedUpdate で行う
+        m_rb.AddForce(Vector2.right * m_h * m_movePower, ForceMode2D.Force);
+    }
+
+    /// <summary>
+    /// 左右を反転させる
+    /// </summary>
+    /// <param name="horizontal">水平方向の入力値</param>
+    void FlipX(float horizontal)
+    {
+        /*
+         * 左を入力されたらキャラクターを左に向ける。
+         * 左右を反転させるには、Transform:Scale:X に -1 を掛ける。
+         * Sprite Renderer の Flip:X を操作しても反転する。
+         * */
+        m_scaleX = this.transform.localScale.x;
+
+        if (horizontal > 0)
+        {
+            this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
+        else if (horizontal < 0)
+        {
+            this.transform.localScale = new Vector3(-1 * Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
+    }
+}
