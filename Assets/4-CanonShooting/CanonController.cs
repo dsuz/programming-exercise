8676238d6f9f187cbf5fbd65af9ee23c2867c763
D. Suzuki <bboydaisuke@gmail.com>
2021-06-02 00:04:53 +0900

    4週目の内容を追加した

---
+++ Assets/4-CanonShooting/CanonController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 砲台 (Canon) を制御するコンポーネント
+/// </summary>
+public class CanonController : MonoBehaviour
+{
+    /// <summary>砲弾として生成するプレハブ</summary>
+    [SerializeField] GameObject m_shellPrefab = default;
+    /// <summary>砲口を指定する</summary>
+    [SerializeField] Transform m_muzzle = default;
+    AudioSource m_audio = default;
+
+    void Start()
+    {
+        m_audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_audio.Play();
+            Instantiate(m_shellPrefab, m_muzzle.position, this.transform.rotation);
+        }
+    }
+}
---
+++ Assets/4-CanonShooting/ShellController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 砲弾 (Shell) を制御するコンポーネント
+/// </summary>
+public class ShellController : MonoBehaviour
+{
+    /// <summary>発射する速度</summary>
+    [SerializeField] float m_initialSpeed = 5f;
+
+    void Start()
+    {
+        // Rigidbody を取得して発射する
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = this.transform.up * m_initialSpeed;
+    }
+
+    void Update()
+    {
+        // 下に落ちたら自分自身を破棄する
+        if (this.transform.position.y < -10f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 何かにぶつかったら自分自身を破棄する
+        Destroy(this.gameObject);
+    }
+}
