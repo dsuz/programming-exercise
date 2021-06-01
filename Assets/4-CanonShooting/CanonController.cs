1dfaa352f4277132598126569e6d3da65b28d510
D. Suzuki <bboydaisuke@gmail.com>
2021-06-02 00:11:34 +0900

    課題1の解答

--- Assets/4-CanonShooting/CanonController.cs
+++ Assets/4-CanonShooting/CanonController.cs
@@ -7,6 +7,8 @@ public class CanonController : MonoBehaviour
 {
     /// <summary>砲弾として生成するプレハブ</summary>
     [SerializeField] GameObject m_shellPrefab = default;
+    /// <summary>照準（砲台の向く方向）となるオブジェクト</summary>
+    [SerializeField] Transform m_crosshair = default;
     /// <summary>砲口を指定する</summary>
     [SerializeField] Transform m_muzzle = default;
     AudioSource m_audio = default;
@@ -18,6 +20,10 @@ public class CanonController : MonoBehaviour
 
     void Update()
     {
+        // 照準の方向に砲台を向ける
+        Vector2 dir = m_crosshair.transform.position - this.transform.position;
+        this.transform.up = dir;
+
         if (Input.GetButtonDown("Fire1"))
         {
             m_audio.Play();
