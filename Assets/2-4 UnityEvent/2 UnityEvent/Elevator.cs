73cdaf9aee1ddcb01813184a105c3bac4c3722c1
D. Suzuki <bboydaisuke@gmail.com>
2021-08-25 02:05:23 +0900

    課題3, 4の解答

---
+++ Assets/2-4 UnityEvent/2 UnityEvent/Elevator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    Animator m_anim = default;
+    AudioSource m_audio = default;
+    /// <summary>上にあるのか、下に居るのか</summary>
+    bool m_isUp = false;
+    /// <summary>動作中フラグ</summary>
+    bool m_isMoving = false;
+
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+        m_audio = GetComponent<AudioSource>();
+    }
+
+    public void Move()
+    {
+        if (m_isMoving) return;
+        m_isMoving = true;
+        m_audio.Play();
+
+        if (m_isUp)
+        {
+            m_anim.Play("Down");
+        }
+        else
+        {
+            m_anim.Play("Up");
+        }
+
+        m_isUp = !m_isUp;
+    }
+
+    /// <summary>
+    /// 動作中フラグを倒す。アニメーションイベントから呼ぶ。
+    /// </summary>
+    void EndMove()
+    {
+        m_isMoving = false;
+    }
+}
