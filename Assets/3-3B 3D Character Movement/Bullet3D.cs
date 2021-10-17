f762d8c4b06c7fcfe8f6521a58a05de24ec069f0
D. Suzuki <bboydaisuke@gmail.com>
2021-10-17 19:35:59 +0900

    第3ターム3週目Bの内容と教材を追加した

---
+++ Assets/3-3B 3D Character Movement/Bullet3D.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet3D : MonoBehaviour
+{
+    [SerializeField] float _speed = 20f;
+    [SerializeField] float _lifeTime = 30f;
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = this.transform.forward * _speed;
+        Destroy(this.gameObject, _lifeTime);
+    }
+}
---
+++ Assets/3-3B 3D Character Movement/CharacterMove3D.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMove3D : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 1f;
+    [SerializeField] float _jumpSpeed = 1f;
+    Rigidbody _rb = default;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector3 dir = Vector3.right * h;
+        
+        if (dir != Vector3.zero)
+        {
+            this.transform.forward = dir;
+        }
+
+        _rb.velocity = dir * _moveSpeed + _rb.velocity.y * Vector3.up;  // Y 軸方向の速度は変えない
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 velocity = _rb.velocity;
+            velocity.y = _jumpSpeed;
+            _rb.velocity = velocity;
+        }
+    }
+}
