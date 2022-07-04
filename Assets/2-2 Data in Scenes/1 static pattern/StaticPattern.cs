a1de21f858878abcdead33722c6b9b8f5e353244
D. Suzuki <bboydaisuke@gmail.com>
2022-07-04 23:45:13 +0900

    2-3の内容を追加した

--- Assets/2-2 Data in Scenes/1 static pattern/StaticPattern.cs
+++ Assets/2-2 Data in Scenes/1 static pattern/StaticPattern.cs
@@ -10,10 +10,6 @@ public class StaticPattern : MonoBehaviour
     public static string m_name = "ああああ";
     /// <summary>メッセージを表示するテキスト</summary>
     [SerializeField] Text m_text = default;
-    /// <summary>座標を保存するオブジェクトの名前</summary>
-    [SerializeField] string m_targetObjectName = "Bird";
-    /// <summary>保存した座標</summary>
-    static Vector3 m_savedPosition = default;
 
     /// <summary>
     /// 名前を保存する
@@ -24,24 +20,12 @@ public class StaticPattern : MonoBehaviour
         StaticPattern.m_name = input.text;
     }
 
-    /// <summary>
-    /// 座標を保存する
-    /// </summary>
-    public void SavePosition()
-    {
-        GameObject go = GameObject.Find(m_targetObjectName);
-        m_savedPosition = go.transform.position;
-    }
-
     void Start()
     {
         if (m_text)
         {
             m_text.text = $"よくぞ来た！勇者 <b><color=red>{StaticPattern.m_name}</color></b> よ！";
             Debug.Log(m_text.text);
-            // 保存した座標にオブジェクトを移動する
-            GameObject go = GameObject.Find(m_targetObjectName);
-            go.transform.position = m_savedPosition;
         }
     }
 }
